9f7b15c0e1154582c0dc0bc3a86d2c6591f2bf81
Tim Crisp <timothycrisp@gmail.com>
2023-03-27 12:52:32 -0500

    import react router dom, stub up nav components, update app dot js and app dot css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [navItems, setNavItems] = useState([
+    {url: "/jaromir-jagr", name: "Jaromir Jagr"},
+    {url: "/barry-melrose", name: "Barry Melrose"},
+    {url: "/guy-lafleur", name: "Guy Lafleur"},
+    {url: "/paul-newman", name: "Paul Newman"}
+  ])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is our app</h1>
       </header>
     </div>
   );
